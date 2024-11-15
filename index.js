ff28e0ef320c47704c0eeafd1535a6de424a1015
Alan Hu <alanh@ccs.neu.edu>
2024-11-15 17:14:01 -0500

    Commit classwork

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
-const app = express()
-Lab5(app)
-Hello(app)
-app.listen(process.env.PORT || 4000)
+import UserRoutes from "./Kanbas/Users/routes.js";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+Lab5(app);
+Hello(app);
+UserRoutes(app);
+app.listen(process.env.PORT || 4000);
