d8205f1168f6b556880c6b709de430d540201de1
Alan Hu <alanh@ccs.neu.edu>
2024-12-06 07:13:00 -0500

    Try adding namespaces

--- Kanbas/Assignments/model.js
+++ Kanbas/Assignments/model.js
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
 import schema from "./schema.js";
-const model = mongoose.model("AssignmentModel", schema, "assignments");
+const model = mongoose.model("AssignmentModel", schema, "kanbas.assignments");
 export default model;
--- Kanbas/Enrollments/model.js
+++ Kanbas/Enrollments/model.js
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
 import schema from "./schema.js";
-const model = mongoose.model("EnrollmentModel", schema, "enrollments");
+const model = mongoose.model("EnrollmentModel", schema, "kanbas.enrollments");
 export default model;
