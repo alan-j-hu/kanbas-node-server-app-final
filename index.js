5f6309466502ba65c42272a20082f63053611932
Alan Hu <alanh@ccs.neu.edu>
2024-12-06 06:12:26 -0500

    Revert

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import cors from "cors";
 import session from "express-session";
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
-const ORIGIN = process.env.NETLIFY_URL || "http://localhost:3000+blahblahblah";
+const ORIGIN = process.env.NETLIFY_URL || "http://localhost:3000";
 console.log(CONNECTION_STRING);
 console.log(ORIGIN);
 mongoose.connect(CONNECTION_STRING);
